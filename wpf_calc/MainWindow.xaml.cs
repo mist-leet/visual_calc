6069f49a224cc95e8a473701ae91d9014fdf9ad7
ARTEMOV_ILYA <artemov.ilya.r@gmail.com>
2019-07-18 16:36:27 +0300

    new file:   .vs/wpf_calc/v15/.suo 	new file:   .vs/wpf_calc/v15/Server/sqlite3/db.lock 	new file:   .vs/wpf_calc/v15/Server/sqlite3/storage.ide 	new file:   .vs/wpf_calc/v15/Server/sqlite3/storage.ide-shm 	new file:   .vs/wpf_calc/v15/Server/sqlite3/storage.ide-wal new file:   wpf_calc.sln 	new file:   wpf_calc/App.config 	new file:   wpf_calc/App.xaml 	new file:   wpf_calc/App.xaml.cs 	new file:   wpf_calc/MainWindow.xaml 	new file:   wpf_calc/MainWindow.xaml.cs 	new file:   wpf_calc/Properties/AssemblyInfo.cs 	new file:   wpf_calc/Properties/Resources.Designer.cs 	new file:   wpf_calc/Properties/Resources.resx 	new file:   wpf_calc/Properties/Settings.Designer.cs 	new file:   wpf_calc/Properties/Settings.settings 	new file:   wpf_calc/bin/Debug/wpf_calc.exe 	new file:   wpf_calc/bin/Debug/wpf_calc.exe.config 	new file:   wpf_calc/bin/Debug/wpf_calc.pdb 	new file:   wpf_calc/lib.cs 	new file:   wpf_calc/obj/Debug/App.g.cs 	new file:   wpf_calc/obj/Debug/App.g.i.cs 	new file:   wpf_calc/obj/Debug/DesignTimeResolveAssemblyReferences.cache 	new file:   wpf_calc/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   wpf_calc/obj/Debug/MainWindow.baml 	new file:   wpf_calc/obj/Debug/MainWindow.g.cs 	new file:   wpf_calc/obj/Debug/MainWindow.g.i.cs 	new file:   wpf_calc/obj/Debug/TempPE/Properties.Resources.Designer.cs.dll 	new file:   wpf_calc/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   wpf_calc/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   wpf_calc/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs 	new file:   wpf_calc/obj/Debug/wpf_calc.Properties.Resources.resources 	new file:   wpf_calc/obj/Debug/wpf_calc.csproj.CoreCompileInputs.cache 	new file:   wpf_calc/obj/Debug/wpf_calc.csproj.FileListAbsolute.txt 	new file:   wpf_calc/obj/Debug/wpf_calc.csproj.GenerateResource.cache 	new file:   wpf_calc/obj/Debug/wpf_calc.csprojAssemblyReference.cache 	new file:   wpf_calc/obj/Debug/wpf_calc.exe 	new file:   wpf_calc/obj/Debug/wpf_calc.g.resources 	new file:   wpf_calc/obj/Debug/wpf_calc.pdb 	new file:   wpf_calc/obj/Debug/wpf_calc_MarkupCompile.cache 	new file:   wpf_calc/obj/Debug/wpf_calc_MarkupCompile.i.cache 	new file:   wpf_calc/wpf_calc.csproj 	new file:   wpf_calc/wpf_calc.csproj.user

---
+++ wpf_calc/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace wpf_calc
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            text_box_qst.TextChanged += TextChanged;
+        }
+
+        private void TextChanged(object sender, RoutedEventArgs e)
+        {
+            string s = ((TextBox)sender).Text;
+
+            if (CheckSpell((TextBox)sender))
+            {
+                Expression current_exp = new Expression(s);
+                if (current_exp.Parsed)
+                {
+                    text_box_ans.Text = Convert.ToString(current_exp.Calc()) + "\n" + current_exp.ToString();
+                }
+                else
+                {
+                    text_box_ans.Text = "error\n" + current_exp.ToString();
+                }
+            }
+            else
+                text_box_ans.Text = "error";
+        }
+
+        private bool CheckSpell(TextBox t)
+        {
+            string s = t.Text;
+            if (s == "")
+                return false;
+            for(int i = 0; i < s.Length; i++)
+                if (!(CheckChar(s[i])))
+                    return false;
+            return true;
+        }
+
+        private bool CheckChar(char q)
+        {
+            if (q >= '0' && q <= '9')
+                return true;
+            if (q == ' ')
+                return true;
+            if (q == '(' || q == ')')
+                return true;
+            if (q == '+' || q == '-' ||
+                q == '*' || q == '/')
+                return true;
+            return false;
+                
+        }
+    }
+
+}
---
+++ wpf_calc/lib.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+interface IExpression
+{
+    int Calc();
+
+}
+
+class Expression : IExpression
+{
+    private char[] valid_bin_ops = { '+', '-', '*', '/' };
+
+    private IExpression[] exps;
+
+    private int size = 0;
+
+    private bool parsed;
+
+    public bool Parsed
+    {
+        get
+        {
+            return parsed;
+        }
+    }
+
+    public int Calc()
+    {
+        if (!parsed)
+            return 0;
+        int result = 0;
+        foreach (IExpression e in exps)
+        {
+            result += e.Calc();
+        }
+        return result;
+    }
+
+    public Expression(string s)
+    {
+        parsed = true;
+        s = DeleteAllSpaces(s);
+        s = DeleteExternalBrackets(s);
+        ParseExpression(s);
+    }
+
+    public Expression(IExpression[] es)
+    {
+        Array.Resize<IExpression>(ref exps, es.Length);
+        for (int i = 0; i < es.Length; i++)
+            exps[i] = es[i];
+    }
+
+    public Expression(IExpression e)
+    {
+
+        Array.Resize<IExpression>(ref exps, 1);
+        exps[0] = e;
+    }
+
+    public void ParseExpression(string s)
+    {
+        try
+        {
+            if (!IsValue(s))
+            {
+                foreach (char op in valid_bin_ops)
+                {
+                    int i = FindSym(s, op);
+                    if (i != -1)
+                    {
+                        AddExpression(s, i, ref exps, ref size);
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                AddExpression(s, ref exps, ref size);
+            }
+        }
+        catch (IndexOutOfRangeException e)
+        {
+            parsed = false;
+            return;
+
+        }
+    }
+
+    public override string ToString()
+    {
+        if (parsed)
+            return exps[0].ToString();
+        else
+            return "unparsed";
+    }
+
+    public bool IsValueExp()
+    {
+        if (parsed && exps.Length == 1 && exps[0] is Value)
+            return true;
+        else
+            return false;
+    }
+
+    // Parse methods
+    private bool IsValue(string s)
+    {
+        foreach (char c in s)
+            foreach (char op in valid_bin_ops)
+                if (c == op)
+                    return false;
+        return true;
+    }
+
+    private void AddExpression(string s, int i, ref IExpression[] exps, ref int size)
+    {
+        if (i == 0)
+            if (s[i] == '-')
+            {
+                size++;
+                Array.Resize<IExpression>(ref exps, size);
+                exps[size - 1] = new Substruction(new Expression("0"),
+                                                  new Expression(s.Substring(1, s.Length - 1)));
+                return;
+            }
+            else if (s[i] == '+')
+            {
+                size++;
+                Array.Resize<IExpression>(ref exps, size);
+                exps[size - 1] = new Expression(s.Substring(1, s.Length - 1));
+            }
+        
+        
+        Expression exp1 = new Expression(s.Substring(0, i));
+        Expression exp2 = new Expression(s.Substring(i + 1, s.Length - i - 1));
+
+        size++;
+        Array.Resize<IExpression>(ref exps, size);
+
+        switch (s[i])
+        {
+            case '+':
+                {
+                    exps[size - 1] = new Addition(exp1, exp2);
+                    break;
+                }
+            case '-':
+                {
+                    exps[size - 1] = new Substruction(exp1, exp2);
+                    break;
+                }
+            case '*':
+                {
+                    exps[size - 1] = new Multiply(exp1, exp2);
+                    break;
+                }
+            case '/':
+                {
+                    exps[size - 1] = new Division(exp1, exp2);
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+
+    private void AddExpression(string s, ref IExpression[] exps, ref int size)
+    {
+        s = DeleteAllBrackets(s);
+        size++;
+        Array.Resize<IExpression>(ref exps, size);
+        exps[size - 1] = new Value(Int32.Parse(s));
+    }
+
+    // String methods
+    private string DeleteAllBrackets(string s)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '(' || s[i] == ')')
+                s = s.Remove(i, 1);
+        }
+        return s;
+    }
+
+    private string DeleteAllSpaces(string s)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == ' ')
+                s = s.Remove(i, 1);
+        }
+        return s;
+    }
+
+    private string DeleteExternalBrackets(string s)
+    {
+        while (s[0] == '(' && s[s.Length - 1] == ')')
+        {
+            s = s.Remove(0, 1);
+            s = s.Remove(s.Length - 1, 1);
+        }
+        return s;
+    }
+
+    private int SkipBrackets(string s, int index)
+    {
+        int i = index;
+        while (s[i] != ')')
+        {
+            i++;
+            if (s[i] == ')')
+                return i + 1;
+            else if (s[i] == '(')
+                i = SkipBrackets(s, i + 1);
+        }
+        return 0;
+    }
+
+    private int FindSym(string s, char op)
+    {
+        int i;
+        for (i = 0; i < s.Length; i++)
+            if (s[i] == op)
+                return i;
+            else if (s[i] == '(')
+                i = SkipBrackets(s, i);
+        return -1;
+    }
+
+    public static string AddExternalBrackets(string s)
+    {
+        if (!(s[0] == '(' && s[s.Length - 1] == ')'))
+        {
+            s = "(" + s + ")";
+        }
+        return s;
+    }
+}
+
+class Value : IExpression
+{
+    int value;
+
+    public int Calc()
+    {
+        return value;
+    }
+
+    public Value(int a)
+    {
+        value = a;
+    }
+
+    public override string ToString()
+    {
+        return value.ToString();
+    }
+}
+
+class Addition : IExpression
+{
+    IExpression exp1;
+    IExpression exp2;
+
+    public int Calc()
+    {
+        return exp1.Calc() + exp2.Calc();
+    }
+
+    public Addition(IExpression e1, IExpression e2)
+    {
+        exp1 = e1;
+        exp2 = e2;
+    }
+
+    public override string ToString()
+    {
+        return Expression.AddExternalBrackets(exp1.ToString() + " + " + exp2.ToString());
+    }
+}
+
+class Substruction : IExpression
+{
+    IExpression exp1;
+    IExpression exp2;
+
+    public int Calc()
+    {
+        return exp1.Calc() - exp2.Calc();
+    }
+
+    public Substruction(IExpression e1, IExpression e2)
+    {
+        exp1 = e1;
+        exp2 = e2;
+
+        if (!(((Expression)exp2).IsValueExp()) && ((Expression)exp2).Parsed)
+            exp2 = Negative(exp2);
+    }
+
+    public override string ToString()
+    {
+        return Expression.AddExternalBrackets(exp1.ToString() + " - " + exp2.ToString());
+    }
+
+    public IExpression Negative(IExpression exp)
+    {
+        IExpression e = new Expression("0");
+        
+        return new Expression(new Substruction(e, exp));
+    }
+}
+
+class Multiply : IExpression
+{
+    IExpression exp1;
+    IExpression exp2;
+
+    public int Calc()
+    {
+        return exp1.Calc() * exp2.Calc();
+    }
+
+    public Multiply(IExpression e1, IExpression e2)
+    {
+        exp1 = e1;
+        exp2 = e2;
+    }
+
+    public override string ToString()
+    {
+        return Expression.AddExternalBrackets(exp1.ToString() + " * " + exp2.ToString());
+    }
+}
+
+class Division : IExpression
+{
+    IExpression exp1;
+    IExpression exp2;
+
+    public int Calc()
+    {
+        return exp1.Calc() / exp2.Calc();
+    }
+
+    public Division(IExpression e1, IExpression e2)
+    {
+        exp1 = e1;
+        exp2 = e2;
+    }
+
+    public override string ToString()
+    {
+        return Expression.AddExternalBrackets(exp1.ToString() + " / " + exp2.ToString());
+    }
+}
+
